8c8e2b7291f5dbb1bfe68cf8020db5680f16f579
Daniel Wieczorek <danielwieczorek901@gmail.com>
2023-10-30 11:22:08 +0100

    added button component

---
+++ src/components/Button.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Button = ({ styles }) => {
+    return (
+        <button
+            type="button"
+            className={`py-4 px-6 bg-blue-gradient
+                font-poppins font-medium text-[18px]
+                text-primay outline-none ${styles} rounded-[10px]
+            `}
+        >
+            Get Started
+        </button>
+    )
+}
+
+export default Button
