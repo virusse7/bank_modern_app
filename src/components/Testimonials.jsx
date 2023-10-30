f5d39676efb4bffc56a3d0a95b7a6aef24127afe
Daniel Wieczorek <danielwieczorek901@gmail.com>
2023-10-30 14:42:51 +0100

    updated word relative

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -7,7 +7,7 @@ import FeedbackCard from "./FeedbackCard";
 const Testimonials = () => (
     <section
         id="clients"
-        className={`${styles.paddingY} ${styles.flexCenter} flex-col realtive`}
+        className={`${styles.paddingY} ${styles.flexCenter} flex-col relative`}
     >
         <div className="absolute z-[0] w-[60%] h-[60%] -right-[50%] rounded-full blue__gradient" />
         <div
